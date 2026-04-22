99492ef132899833042cfa468668c70db110639d
jnavno <jnn@suntek.dev>
2026-04-22 15:58:02 +0200

    polished testimonial photo and name

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -2,7 +2,7 @@ import type { Testimonials } from "@interfaces/ITestimonials"
 
 export const testimonials: Testimonials = {
   sophiaGallac: {
-    fullName: "Montse Navarro",
+    fullName: "Sophia Gallac",
     testimonial: "Em sentia perduda en els meus propis pensaments, aclaparada per l'ansietat i la inseguretat. Començar la teràpia amb la Clara ho va canviar tot. La seva guia m'ha ajudat a retrobar la confiança en mi mateixa i a establir límits saludables. Ara afronto els reptes amb la ment calmada i finalment sento que tinc el control de la meva vida. No tinc paraules per agrair-li tot el que ha fet per mi!"
   }
 }
